db118f3b09100a819ddb6e64abcab63eaa69ded2
RCYGM <jurgenmuller997@gmail.com>
2025-01-12 13:27:09 +0100

    feat: post /api/users and get /api/users

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
-require("dotenv");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+let userid = 0;
+const users = [];
 
 app.use(cors());
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+});
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+app.post("/api/users", (req, res) => {
+  const username = req.body.username;
+  const id = userid++;
+  users.push({ username: username, _id: id });
+  res.json({ username: username, _id: id });
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
